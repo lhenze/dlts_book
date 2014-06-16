b254d88f004bf790d18ddf1734fa9b7e0935d3f1
Laura Henze <laurahenze@gmail.com>
2014-06-16 13:09:46 -0400

    wrapper div around languages.
    removed related titles label from multivol drop down

--- templates/node--dlts-book-metadata.tpl.php
+++ templates/node--dlts-book-metadata.tpl.php
@@ -1,9 +1,17 @@
 <div dir="<?php print $lang_dir ?>" data-dir="<?php print $lang_dir ?>" data-lang="<?php print $lang_language ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php if (isset($lang_options)) : ?><?php print t('Available languages:') ?> <?php print render($lang_options) ; ?><?php endif; ?>
-  <?php print render($content); ?>
-  <?php if (isset($select_multivolbook)) : ?>
+
+
+   <?php if (isset($lang_options)) : ?><div class="lang-options"><?php print t('Available languages:') ?> <?php print render($lang_options) ; ?></div><?php endif; ?>
+ 
+
+  <?php  print render($content); ?>
+ 
+   <?php if (isset($select_multivolbook)) : ?>
     <div class="field field-name-field-title field-type-text field-label-inline clearfix">
-      <div class="field-label"><?php print t('Related titles') ?>:</div><?php print $select_multivolbook; ?>
+      <?php print $select_multivolbook; ?>
     </div>
-  <?php endif; ?>  
+  <?php endif; ?>
+
 </div>
+
+  
