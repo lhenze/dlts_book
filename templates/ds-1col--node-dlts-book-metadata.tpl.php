d6a9d0d69f423b7b2f95a268a6dd29690dfe544a
Laura Henze <laurahenze@gmail.com>
2015-01-23 12:47:31 -0500

    Conditional was closed incorrectly.

--- templates/ds-1col--node-dlts-book-metadata.tpl.php
+++ templates/ds-1col--node-dlts-book-metadata.tpl.php
@@ -9,9 +9,10 @@
   <?php if (isset($lang_options)) : ?><div class="lang-options"><?php print locale('Available languages', NULL, $lang_language) ?>: <?php print render($lang_options) ; ?></div><?php endif; ?>
     <?php if (isset($select_multivolbook)) : ?>
    			<?php print $select_multivolbook; ?>
+   	 <?php endif; ?>
   	<?php print $ds_content; ?>
  
-  <?php endif; ?>
+ 
 </<?php print $ds_content_wrapper ?>>
 <?php if (!empty($drupal_render_children)): ?>
   <?php print $drupal_render_children ?>
